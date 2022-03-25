e57a1f3acdbe85a9baef8f873de1364d2b7c133c
jeroee <jeroejeroe@gmail.com>
2022-03-25 11:13:15 +0800

    set up backend basic boiler plate

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+// import postRoutes from './routes/posts.js';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello to test API');
+})
+
+const CONNECTION_URL = process.env.ATLAS_URI;
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT} with mongoose connected`)))
+    .catch((err) => console.log(err.message));
