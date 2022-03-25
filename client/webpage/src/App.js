ce2b2747d70d9d5129e9e27f90c17a6bd37c49c2
estherc247 <estherc2499@gmail.com>
2022-03-25 13:27:11 +0800

    new link

--- client/webpage/src/App.js
+++ client/webpage/src/App.js
@@ -4,15 +4,19 @@ import Home from "./components/pages/Home";
 import Account from "./components/pages/Account";
 import Loans from "./components/pages/Loans";
 import Login from "./components/pages/Login";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<SignUp />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+        //<Route path="/" element={<Home />} />
         <Route path="/account" element={<Account />} />
         <Route path="/loans" element={<Loans />} />
-        <Route path="/Login" element={<Login />} />
+        //<Route path="/Login" element={<Login />} />
       </Routes>
       <div> footer </div>
     </Router>
