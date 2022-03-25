e30095151853f7f49b41912c038cbbbae64e080b
simon <SWONG045@e.ntu.edu.sg>
2022-03-25 13:11:48 +0800

    Update

--- client/webpage/src/components/CurrentLoans.js
+++ client/webpage/src/components/CurrentLoans.js
@@ -1,8 +1,22 @@
 import React from 'react'
+// import "./CurrentLoans.css"
 
 const CurrentLoans = () => {
+
   return (
-    <div>CurrentLoans</div>
+    <section className='currentloans'>
+        <div className="currentloans--content">
+                <h1>Current Loans</h1>
+        </div>
+
+        <div className="currentloans--container">
+        
+        <p>Loan ID</p>
+        <p>Loan Amount</p>
+        <p>Pay Loans</p>
+
+        </div>
+    </section>
   )
 }
 
