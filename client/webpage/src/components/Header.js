222696463b7a1d07c6f083d6b9c03e7113a3be44
simon <SWONG045@e.ntu.edu.sg>
2022-03-25 11:11:19 +0800

    Frontend Run

--- client/webpage/src/components/Header.js
+++ client/webpage/src/components/Header.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Header.css";
+import logo from "./assets/logo.jpg";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [active, setActive] = useState(false);
 
-export default Header
+  function toggle() {
+    setActive(!active);
+  }
+
+  return (
+    <div className="header">
+      <div className="header--logo">
+        <img src={logo} alt="" />
+      </div>
+
+      <div className="menu--icon">
+        <MenuIcon className="menu" onClick={toggle} />
+      </div>
+
+      <nav>
+        <ul className={active ? "list--items active" : "list--items"}>
+          <div className="menu--close">
+            <CloseIcon className="close" onClick={toggle} />
+          </div>
+
+          <li>
+            <h1>Home</h1>
+          </li>
+
+          <li>
+            <h1>Loans</h1>
+          </li>
+
+          <li>
+            <h1>Login/SignUp</h1>
+          </li>
+
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
