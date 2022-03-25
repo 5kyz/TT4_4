e8cd8f4c30128e96b62a7024aae5dbccdeb0f420
estherc247 <estherc2499@gmail.com>
2022-03-25 13:40:28 +0800

    login start

--- client/webpage/src/App.js
+++ client/webpage/src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/pages/Home";
-import Account from "./components/pages/Account";
-import Loans from "./components/pages/Loans";
 import Login from "./components/pages/Login";
 import SignUp from "./components/pages/SignUp";
 
@@ -10,15 +8,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Home" element={<Home />} />
-        //<Route path="/" element={<Home />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/loans" element={<Loans />} />
-        //<Route path="/Login" element={<Login />} />
       </Routes>
-      <div> footer </div>
     </Router>
   );
 }
