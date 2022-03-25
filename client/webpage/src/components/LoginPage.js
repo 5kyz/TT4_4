5dc25e9bb77092f88bf247b13d6e152fe2c6684f
estherc247 <estherc2499@gmail.com>
2022-03-25 13:12:57 +0800

    sign and log UI

---
+++ client/webpage/src/components/LoginPage.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect, Component, useRef} from 'react';
+import { useNavigate } from 'react-router-dom';
+import './LoginPage.css';
+//import axios from 'axios';
+
+export default function LoginPage (){
+  const [password, setPassword] = useState();
+  const [userPhone, setUserPhone] = useState();
+
+  const [allowLogin, setAllowLogin] = useState(false);
+
+  function handleclick(){
+    setAllowLogin(true)
+
+  }
+  return(
+    <>
+    <div className='outer'>
+
+        <div className='login-header'>
+          <h2> Welcome to DBS </h2>
+          <img src='assets/logo.jpg' className='dbs-logo' width='400' height='100'></img>
+        </div>
+
+        <div className='login-cred'>
+            <form className='log-form'>
+              <label>
+                <p>Phone Number</p>
+                <input type='text' onChange={e=>setUserPhone(e.target.value)}/>
+              </label>
+              <label>
+                <p>Password</p>
+                <input type="password" onChange={e=>setPassword(e.target.value)}/>
+              </label>
+              <div>
+                <button className='log-btn' onClick={handleclick}>Login</button>
+              </div>
+
+              {allowLogin ? (<p>Welcome! Login Successful</p>) : (<p>Sorry please try again.</p>)}
+
+            </form>
+          </div>
+      </div>
+    </>
+  )
+}
---
+++ client/webpage/src/components/SignUpPage.js
@@ -0,0 +1,66 @@
+import React, {useState, useEffect, Component, useRef} from 'react';
+import {Link } from 'react-router-dom';
+import './SignUpPage.css';
+
+export default function SignUpPage (){
+  const [userName, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const [checkpwd, setCheckPwd] = useState();
+  const [userAddr, setUserAddr] = useState();
+  const [userPhone, setUserPhone] = useState();
+
+  const [allowSignUp, setAllowSignUp] = useState(false);
+
+  function signup(){
+
+  }
+  return(
+    <>
+    <div className='outer'>
+
+        <div className='login-header'>
+          <h2> Welcome to DBS </h2>
+          <img src='assets/logo.jpg' className='dbs-logo' width='400' height='100'></img>
+        </div>
+
+        <div className='login-cred'>
+            <form className='log-form'>
+              <label>
+                <p>Name</p>
+                <input type="text" onChange={e=>setUserName(e.target.value)}/>
+              </label>
+              <label>
+                <p>Phone Number</p>
+                <input type='text' onChange={e=>setUserPhone(e.target.value)}/>
+              </label>
+              <label>
+                <p>Address</p>
+                <input type='text'onChange={e=>setUserAddr(e.target.value)}/>
+              </label>
+              <label>
+                <p>Password</p>
+                <input type="password" onChange={e=>setPassword(e.target.value)}/>
+              </label>
+              <label>
+                <p>Re-Enter Password</p>
+                <input type="password" onChange={e=>setCheckPwd(e.target.value)}/>
+              </label>
+
+              <div className='reg-btns'>
+                <div>
+                  <button type="submit" onClick={signup}>SignUp</button>
+                </div>
+                <div>
+                  <Link to='/Login' className='log-btn'>Click here to login</Link>
+                  {/*<button className='log-btn' onClick={handleclick}> Click here to Login </button>*/}
+                </div>
+              </div>
+
+              {allowSignUp ? (<p>Welcome! SignUp Successful</p>) : (<p>Sorry password mismatch please try again.</p>)}
+
+            </form>
+          </div>
+      </div>
+    </>
+  )
+}
